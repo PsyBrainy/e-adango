6d06ac5ca9d14f482db5d1a11827f2681b347a57
PsyBrainy <martuto93@gmail.com>
2020-12-01 14:21:19 -0300

    init

---
+++ src/main/java/com/psybrainy/eadango/EAdangoApplication.java
@@ -0,0 +1,13 @@
+package com.psybrainy.eadango;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EAdangoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EAdangoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/psybrainy/eadango/EAdangoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.psybrainy.eadango;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EAdangoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
